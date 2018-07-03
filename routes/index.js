e81edd00c941f9be1f963d3e42cfd039694e0680
Jimmy K <jwkellenberger@gmail.com>
2018-07-03 14:17:21 +0000

    Added routes for site navigation and sign in posts

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var User          = require('../models/user'),
+    passport      = require('passport'),
+    express       = require('express'),
+    router        = express.Router();
+
+//Root Route
+router.get('/', function(req, res){
+    res.render('campgrounds/landing');
+});//MONGO DB CRUD -> //insert(), find(), update(), remove()
+
+
+
+// = Auth Routes =
+
+//Register Form Route
+router.get('/register', function(req, res){
+    res.render('register');
+});
+
+//Handle Sign Up Logic
+router.post('/register', function(req, res){
+    //Auth Stuff
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            req.flash('error', err.message); //present passport error
+            return res.redirect('register');
+            //need feedback to user as to error..
+        } else{
+            passport.authenticate('local')(req,res, function(){
+               req.flash('success', 'Welcome to WeCamp: '+ user.username);
+               res.redirect('/campgrounds'); 
+            });
+        }
+    });
+    //res.send('signing up!');
+    //evential
+    //res.redirect('/');
+});
+
+// login with 2 routes: (get => form, post => login)
+//Show Login Form
+router.get('/login', function(req, res){
+    res.render('login');
+});
+
+
+//using setup for passport authentication for logging in!
+//Handle Login Logic
+router.post('/login', passport.authenticate('local', 
+    {   successRedirect: '/campgrounds',
+        failureRedirect: '/login',
+        failureFlash: "Invalid username or password",
+        successFlash: "Welcome to WeCamp!"
+    }), function(req, res){
+    //generic callback
+});
+
+//Handle Logout Logic
+router.get('/logout', function(req, res){
+    req.logout(); // comes from combo of passport and expres-sessions
+    req.flash('success', 'Logged you out! Cheerio!');
+    res.redirect('/campgrounds');
+})
+
+module.exports = router;

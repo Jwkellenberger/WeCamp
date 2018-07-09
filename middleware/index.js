9b9b986aec73e32cdb66d0cf6e82fdc6eb0dd884
Jimmy K <jwkellenberger@gmail.com>
2018-07-09 19:53:57 +0000

    Added admin option to edit/delete comments And campgrounds

--- middleware/index.js
+++ middleware/index.js
@@ -35,7 +35,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
                 //message campground not found
             } else{
                 //does user own the campground?
-                if(foundComment.author.id.equals(req.user._id)){
+                if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
                     next();
                 } else {
                     req.flash('error', 'You do not have permission for that operation!');

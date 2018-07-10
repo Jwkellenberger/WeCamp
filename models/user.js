18d654d52848c52ee83f4ab8721c16fe716f6c99
Jimmy K <jwkellenberger@gmail.com>
2018-07-10 03:06:41 +0000

    Changed user schema to allow for password reset

--- models/user.js
+++ models/user.js
@@ -4,12 +4,14 @@ var passportLocalMongoose = require('passport-local-mongoose');
 //Set up user Model
 var userSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
+    password: String,
     avatar: String,
     firstName: String,
     lastName: String,
     email: {type: String, required: true},//unique: true, required: true},
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 //Add Auth functionality from passport for mongoose!
